55edc646fb4bc0e80def6a6f54771f88d841041e
surya-sanity <surya.sanity@gmail.com>
2023-06-10 14:20:34 +0530

    save changes mm

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import * as tf from "@tensorflow/tfjs"
+import * as handpose from "@tensorflow-models/handpose"
+import Webcam from 'react-webcam';
+import { useEffect, useRef } from 'react';
+import { drawHand } from './helper';
 
 function App() {
+  const webCamRef = useRef(null)
+  const canvasRef = useRef(null)
+
+  const runHandPose = async () => {
+    const net = await handpose.load();
+    console.log('handpose model loaded !')
+    return net;
+  }
+  const detectHand = async (net) => {
+    if (typeof webCamRef.current !== 'undefined' && webCamRef.current !== null && webCamRef.current.video.readyState === 4) {
+      const video = webCamRef.current.video;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
+
+      webCamRef.current.video.width = videoWidth;
+      webCamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+
+      const hand = await net.estimateHands(video);
+      console.log("hand", hand);
+
+      const ctx = canvasRef.current.getContext('2d');
+      drawHand(hand, ctx);
+    }
+  }
+
+  useEffect(() => {
+    let interval;
+    runHandPose().then((net) => {
+      interval = setInterval(() => {
+        detectHand(net);
+      }, 100)
+    })
+    return () => clearInterval(interval);
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam ref={webCamRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480
+          }}
+        />
+        <canvas ref={canvasRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480
+          }}
+        />
       </header>
     </div>
   );
